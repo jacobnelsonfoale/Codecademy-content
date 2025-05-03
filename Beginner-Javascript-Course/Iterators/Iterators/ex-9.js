d63441da918278d56346262edeba854af5c0028f
Jacob <78283092+jacobnelsonfoale@users.noreply.github.com>
2025-05-03 06:48:33 +0000

    Add files via upload

---
+++ Beginner-Javascript-Course/Iterators/Iterators/ex-9.js
@@ -0,0 +1,29 @@
+/*Review
+
+Awesome job on clearing the
+Preview: Docs Loading link description
+iterators
+lesson! You have learned a number of useful
+Preview: Docs Loading link description
+methods
+in this lesson as well as how to use the JavaScript documentation from the Mozilla Developer Network to discover and understand additional methods. Let’s review!
+
+    Preview: Docs Loading link description
+    .forEach()
+    is used to execute the same code on every element in an array but does not change the array and returns undefined.
+    Preview: Docs Loading link description
+    .map()
+    executes the same code on every element in an array and returns a new array with the updated elements.
+    Preview: Docs Loading link description
+    .filter()
+    checks every element in an array to see if it meets certain criteria and returns a new array with the elements that return truthy for the criteria.
+    Preview: Docs Returns the first index that passes the callback function's test. Returns -1 if no element passes the test.
+    .findIndex()
+    returns the index of the first element of an array that satisfies a condition in the callback function. It returns -1 if none of the elements in the array satisfies the condition.
+    Preview: Docs Loading link description
+    .reduce()
+    iterates through an array and takes the values of the elements and returns a single value.
+    All iterator methods take a callback function, which can be a pre-defined function, a function expression, or an arrow function.
+    You can visit the Mozilla Developer Network to learn more about iterator methods (and all other parts of JavaScript!).
+
+*/
